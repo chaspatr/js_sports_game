7c4bbd435708b06b8539783111b876e9af599e35
Chase Patrick <chasepatrick117@gmail.com>
2020-05-02 17:48:56 -0400

    added the given html and css to the correct files and linked the css file and js file to index.html, as well as declare all the variables and added an event listener to the reset button

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+//declaring variables at the start
+
+let resetButton = document.querySelector('#reset-button');
+
+let resetCounter = document.querySelector('#num-resets');
+
+let teamOneNumShots = document.querySelector('#teamone-numshots');
+
+let teamOneNumGoals = document.querySelector('#teamone-numbgoals');
+
+let teamOneShootButton = document.querySelector('#teamone-shoot-button');
+
+let teamTwoNumShots = document.querySelector('#teamtwo-numshots');
+
+let teamTwoNumGoals = document.querySelector('#teamtwo-numgoals');
+
+let teamTwoShootButton = document.querySelector('#teamtwo-shoot-button');
+
+resetButton.addEventListener("click", function() {
+ console.log("reset button clicked")
+});
+
+resetButton.addEventListener("click", function() {
+
+});
+
+resetButton.addEventListener("click", function() {
+
+});
